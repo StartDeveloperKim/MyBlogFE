56e8b8fb3a19d917dd998596ad365f5b133ad48f
StartDeveloperKim <ktw550111@naver.com>
2023-01-13 17:44:43 +0900

    Feat. 게시글 에디터 뷰 완성

--- myblog/src/views/board/BoardEditView.vue
+++ myblog/src/views/board/BoardEditView.vue
@@ -1,67 +1,113 @@
 <template>
-  <div class="edit-backgroud">
-    <v-container fluid align-items="center">
+  <v-app>
       <div>
-        <v-row align="end">
-          <v-col cols="8" align-self="center">
-            <v-text-field
-              label="TITLE"
-              :rules="titleRules"
-              solo
-            >
-            </v-text-field>
-          </v-col>
-          <v-col cols="4" align-self="center">
-            <v-file-input
-              label="THUMBNAIL"
-              placeholder="THUMBNAIL"
-              prepend-icon="mdi-paperclip"
-              solo
-              :show-size="1000"
-            >
-            </v-file-input>
-          </v-col>
-        </v-row>
+      <v-container fluid align-items="center">
+        <div>
+          <v-row align="end">
+            <v-col cols="8" align-self="center">
+              <v-text-field
+                label="TITLE"
+                :rules="titleRules"
+                v-model="title"
+                solo
+              >
+              </v-text-field>
+            </v-col>
+            <v-col cols="4" align-self="center">
+              <v-file-input
+                label="THUMBNAIL"
+                placeholder="THUMBNAIL"
+                prepend-icon="mdi-paperclip"
+                solo
+                :show-size="1000"
+                @change="thumbnailUpload"
+              >
+              </v-file-input>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="8">
+              <Tagify :tags="tags" placeholder=" TAG"></Tagify>
+            </v-col>
+            <v-col cols="4">
+              <v-select
+                :items="categoryList"
+                item-text="name"
+                item-value="id"
+                label="CATEGORY"
+                v-model="categoryId"
+                :rules="categoryRule"
+                solo
+              ></v-select>
+            </v-col>
+          </v-row>
+        </div>
         <v-row>
-          <v-col cols="8">
-            <Tagify :tags="tags" placeholder=" TAG"></Tagify>
-          </v-col>
-          <v-col cols="4">
-            <v-select
-              :items="item"
-              label="CATEGORY"
-              solo
-            ></v-select>
+          <v-col cols="12">
+            <ToastEditor ref="toastuiEditor" class="toast-editor-backgroud"/>
           </v-col>
         </v-row>
-      </div>
-      <v-row>
-        <v-col cols="12">
-          <Editor ref="toastuiEditor" class="toast-editor-backgroud"/>
-        </v-col>
-      </v-row>
-      <v-btn @click="save">저장하기</v-btn>
-    </v-container>
-  </div>
+        <div class="mt-5">
+          <v-btn @click="save" depressed x-large color="primary" class="font-weight-bold">저장하기</v-btn>
+          <v-btn @click="cancel" depressed x-large color="error" class="font-weight-bold">취소하기</v-btn>
+        </div>
+      </v-container>
+    </div>
+  </v-app>
 </template>
 <script>
 import '@toast-ui/editor/dist/toastui-editor.css'
-import { Editor } from '@toast-ui/vue-editor'
+import ToastEditor from '../editor/ToastEditor.vue'
 import Tagify from 'tagify-vue'
 
 export default {
   components: {
-    Editor, Tagify
+    ToastEditor, Tagify
   },
   methods: {
     save () {
       const content = this.$refs.toastuiEditor.invoke('getHTML')
-      alert(content)
+      const title = this.title
+      const tags = this.tags
+      const categoryId = this.categoryId
+      const thumbnail = this.thumbnail
+
+      if (title === null) {
+        alert('제목을 입력해주세요')
+      } else if (categoryId === null) {
+        alert('카테고리를 선택해주세요')
+      }
+
+      const requestData = {
+        title: title,
+        content: content,
+        categoryId: categoryId,
+        thumbnail: thumbnail,
+        tags: tags
+      }
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      alert(tags)
+      this.$axios.post(this.url, requestData, config)
+        .then((response) => {
+          window.location.href = this.url + '/' + response.data
+        })
+        .catch(error => {
+          alert(error)
+        })
+    },
+    cancel () {
+      if (confirm('작성 중인 글이 저장되지 않았을 수도 있습니다.')) {
+        window.history.back()
+      }
     },
     getBoardEditViewInfo () {
       this.$axios({
         method: 'GET',
-        url: 'http://localhost:8080/board/edit'
+        url: this.url + '/edit'
       }).then((response) => {
         this.temporalBoard = response.data.temporalBoardResp
         if (this.temporalBoard !== null) {
@@ -71,16 +117,33 @@ export default {
           console.log(this.categoryList)
         }
       })
+    },
+    async thumbnailUpload (file) {
+      const formData = new FormData()
+      formData.append('img', file)
+      const config = { 'Content-Type': 'multipart/form-data' }
+      try {
+        const { data } = await this.$axios.post(this.url + '/thumbnail', formData, config)
+        this.thumbnail = data
+      } catch (e) {
+        alert('썸네일 업로드에 실패하였습니다.')
+      }
     }
   },
   data: () => ({
     titleRules: [
       value => !!value || '제목을 입력해주세요.'
     ],
+    categoryRule: [
+      value => !!value || '카테고리를 선택해주세요.'
+    ],
     tags: [],
     categoryList: null,
     temporalBoard: null,
-    item: ['1', '2', '3']
+    title: null,
+    categoryId: null,
+    thumbnail: null,
+    url: 'http://localhost:8080/board'
   }),
   created () {
     this.getBoardEditViewInfo()
---
+++ myblog/src/views/editor/ToastEditor.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="editor"/>
+</template>
+<script>
+import Editor from '@toast-ui/editor'
+
+export default {
+  data () {
+    return {
+      editor: null,
+      url: 'http://localhost:8080/board'
+    }
+  },
+  mounted () {
+    this.editor = new Editor({
+      el: document.querySelector('#editor'),
+      initialEditType: 'markdown',
+      previewStyle: 'vertical',
+      height: '400px',
+      hooks: {
+        addImageBlobHook: this.addImageBlobHook
+      }
+    })
+  },
+  methods: {
+    async addImageBlobHook (file, setText, type) {
+      try {
+        if (!file) return false
+
+        if (file && file.size > 5242880) {
+          // const size = (file.size / (1000 * 1000).toFixed(1))
+          alert('최대 업로드 사이즈(5MB)를 초과 하였습니다.')
+          return false
+        }
+
+        const formData = new FormData()
+        formData.append('image', file)
+        this.$axios.post(this.url + '/toast', formData)
+          .then((response) => {
+            const imageURL = response.data
+            console.log(imageURL)
+            setText(imageURL, 'image')
+          })
+      } catch (e) {
+        alert('파일 업로드에 실패하였습니다.')
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
